9db5247ef6a36a548a4f19d0aa7ead80174c2187
myWorkshop123 <kartikkumar868@gmail.com>
2019-11-07 21:00:32 +0530

    challenge 20 completed

---
+++ EJS Challenge Starting Files/public/home.js
@@ -0,0 +1,23 @@
+// $(document).ready(function () {
+//     $('.nav-toggle').click(function () {
+//         var collapse_content_selector = $(this).attr('href');
+//         var toggle_switch = $(this);
+//         $(collapse_content_selector).toggle(function () {
+//             if ($(this).css('display') == 'none') {
+//                 toggle_switch.html('Read More');
+//             } else {
+//                 toggle_switch.html('Read Less');
+//             }
+//         });
+//     });
+
+// });
+
+
+function cutShort(value) {
+    for (var count = 0 ; count<100 ; count++) {
+        console.log(value[count]);
+        
+    }
+    
+}
