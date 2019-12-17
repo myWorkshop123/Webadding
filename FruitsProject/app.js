56967f1e103a792f4e3e6e258b7cfa1a9a2d20c0
myWorkshop123 <kartikkumar868@gmail.com>
2019-12-17 10:12:48 +0530

    after vacations homework

--- FruitsProject/app.js
+++ FruitsProject/app.js
@@ -1,23 +1,84 @@
-//jshint esversion:6
-const MongoClient = require('mongodb').MongoClient;
-const assert = require('assert');
+// jshint esversion:6
 
-// Connection URL
-const url = 'mongodb://localhost:27017';
+const MongoClient = require('mongodb').MongoClient; // required Module 
+const assert = require('assert'); // I forgot what it used to do 
 
-// Database Name
-const dbName = 'fruitsDb';
+// connection url 
+const url = 'mongodb://localhost:27017';  // the server address where the mongodb will be running 
 
-// Create a new MongoClient
-const client = new MongoClient(url,{useUnifiedTopology: true});
 
-// Use connect method to connect to the Server
+// database name 
+const dbName = 'fruitsDB';
+
+// Create a new MongoClient 
+
+const client = new MongoClient(url , {useUnifiedTopology:true});  // client is used to connect to the server 
+
+
+
+
+// Used Connect method to connect to the server 
 client.connect(function(err) {
-  assert.equal(null, err);
-  console.log("Connected successfully to server");
+    assert.equal(null,err);
+    console.log("Connected successfully to the server");
 
-  const db = client.db(dbName);
-  console.log("Database Created");
+    const db = client.db(dbName);
 
-  client.close();
+   
+
+   insertDocument(db , function(){
+       findDocuments(db , function(){
+           client.close();
+
+       });
+       
+
+       
+   });
+
+
+
+
 });
+ const insertDocument = function(db , callback) {
+        // Get the documents collection
+        const collection = db.collection('FruitList');
+        collection.insertMany([
+            {_id:1 ,name:'Apple' ,  cost:100 , stock:32 } , 
+            {_id:2,name: 'Pineapple' , cost:150 , stock:44} , 
+            {_id:3 ,name:'Guava' , cost:50 , stock:50}
+        ] , function(err,result) {
+            assert.equal(err , null) ;
+            assert.equal(3 , result.result.n);
+            assert.equal(3,result.ops.length);
+            console.log("Inserted 3 documents into the collection");
+            callback(result);
+            
+            
+        
+        } 
+        
+        );
+
+
+    };
+
+
+const findDocuments = function(db , callback) {
+
+    // Get the documents collection
+    const collection = db.collection('FruitList');
+
+    // Find some documents 
+
+    collection.find({}).toArray(function(err , docs){
+      assert.equal(err , null);
+      console.log("Found the following documents");
+      console.log(docs);
+      callback(docs);
+
+      
+        
+    })
+
+}
