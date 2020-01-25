45818449703303ba66f1860d9ed5fa90046a4463
myWorkshop123 <kartikkumar868@gmail.com>
2020-01-25 14:39:39 +0530

    basic hashing is implemented

--- Secrets/app.js
+++ Secrets/app.js
@@ -1,13 +1,13 @@
 //jshint esversion:6
 
 //TODO register and login tempelates have the type of email in their input method of email not text 
-require('dotenv').config();
+
 const express = require('express');
 const ejs = require('ejs');
 const bodyParser = require('body-parser');
 const app = express();
 const mongoose = require('mongoose');
-const encrypt = require('mongoose-encryption');
+const md5 = require('md5');
 
 
 app.use(express.static('public'));
@@ -32,7 +32,7 @@ const userSchema = new mongoose.Schema({
 
 const secret = process.env.SECRET;
 
-userSchema.plugin(encrypt, { secret: secret , encryptedFields:['password'] });
+
 
 // Database Model
 
@@ -64,7 +64,7 @@ app.post("/register", function (req, res) {
 
     
     const userName = req.body.username;
-    const passWord = req.body.password;
+    const passWord = md5(req.body.password);
 
 
     const new_user = new User({
@@ -87,7 +87,7 @@ app.post("/register", function (req, res) {
 
 app.post("/login", function (req, res) {
     const testUser = req.body.username;
-    const testPass = req.body.password;
+    const testPass = md5(req.body.password);
     User.findOne({ username: testUser }, function (err, foundUser) {
         if (err) {
             console.log('this is the error ',err);
